8b7de34c1bd7c454a1650d73b1060279c025c963
incri <paudelbaibhav0@gmail.com>
2025-02-19 11:42:20 +0545

    first lanchain code with summary

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,24 @@
-if  __name__ == "__main":
+from langchain_core.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+information = ""
+
+if __name__ == "__main__":
     print("Hello Langchain!")
+
+    summary_template = """
+        Given the information {information} about a person, create:
+        1. A short summary.
+        2. Two interesting facts about them.
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain.run(information=information))
