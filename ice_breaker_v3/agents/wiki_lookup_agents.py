9ccf760824050c4daaf9bca8628bccbef14c1ccc
incri <paudelbaibhav0@gmail.com>
2025-02-21 20:26:53 +0545

    initializing agent to do the wiki search for us

---
+++ ice_breaker_v3/agents/wiki_lookup_agents.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import initialize_agent, Tool, AgentType
+
+
+def lookup(name: str) -> str:
+
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0)
+    template = """given the full name {name_of_person} I want you to get it me a link to their wiki search deatil page.
+                your answer should contain only a URL"""
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google wikipedia for person detail",
+            func="?",
+            description="Useful for when you need to get detail about people wikipedia URL",
+        )
+    ]
+
+    agent = initialize_agent(
+        tools=tools_for_agent, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    wikipedia_search_url = agent.run(prompt_template.format_prompt(name_of_person=name))
+
+    return wikipedia_search_url
